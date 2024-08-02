369061bea5758538f2f312f907888936045ef182
robinfube <robin.fube@gmail.com>
2024-08-02 20:52:45 +0545

    edupay

---
+++ Modernize-Nextjs-Free-main(1)/Modernize-Nextjs-Free-main/package/src/app/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const IndexView=({params}:{params:{params:{children:React.ReactNode}}})=>{
+
+    return (
+        <div className="bg-black">
+        <nav className="bg-black p-4">
+        <div className="container mx-auto flex justify-between items-center">
+            <a href="#" className="text-white text-2xl font-bold">EduFee</a>
+            <div className="space-x-4">
+                <Link href="#home" className="text-white">Home</Link>
+                <Link href="#features" className="text-white">Features</Link>
+                <Link href="#pricing" className="text-white">Pricing</Link>
+                <Link href="#contact" className="text-white">Contact</Link>
+            </div>
+        </div>
+    </nav>
+    </div>
+
+    );
+
+
+}
+export default IndexView;
